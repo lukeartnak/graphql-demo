f4b8eb4ae510a70adb03359e6215c097384a2ca0
Luke <lartnak@gmail.com>
2017-06-20 23:20:00 -0700

    switch to postgraphql

--- server.js
+++ server.js
@@ -1,22 +1,12 @@
 const express = require('express')
-const graphql = require('express-graphql')
-const {buildSchema} = require('graphql')
+const {postgraphql} = require('postgraphql')
 
 let app = express()
 
-let schema = buildSchema(`
-  type Query {
-    hello: String
-  }
-`)
-
-let rootValue = {
-  hello() {
-    return 'World!'
-  }
-}
-
-app.use('/graphql', graphql({schema, rootValue, graphiql: true}))
+app.use(postgraphql('postgres://localhost:5432', 'graphql_demo', {
+  graphiql: true,
+  watchPg: true
+}))
 
 app.listen(8000, () => {
   console.log('server listening at http://localhost:8000')
